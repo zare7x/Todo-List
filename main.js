bf6fe915e8eb4dd42c3dddfe3815874aa0f1cebf
zare7x <114315155+zare7x@users.noreply.github.com>
2023-02-07 00:50:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", () => {
+  tasks = JSON.parse(localStorage.getItem("tasks")) || []
+  const newTask = document.querySelector("#add-form")
+  newTask.addEventListener("submit" , e => {
+    e.preventDefault();
+
+    const task = e.target.elements.task.value;
+    tasks.push(task)
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+
+    e.target.reset()
+
+    display()
+  })
+  display()
+})
+
+function display() {
+  const list = document.querySelector("#list")
+  list.innerHTML = ""
+  tasks.forEach(task => {
+    const item = document.createElement("div")
+    item.classList.add("item")
+    const content = document.createElement("div")
+    content.classList.add("content")
+
+    const edits = document.createElement("div")
+    const edit = document.createElement("button")
+    edits.classList.add("edits")
+    edit.classList.add("edit")
+    const deletee = document.createElement("button")
+    deletee.classList.add("delete")
+    content.innerHTML += `<input type="text" value="${task}" readonly>`
+  
+    edit.innerHTML = "Edit"
+    deletee.innerHTML = "Delete"
+    edits.appendChild(edit)
+    edits.appendChild(deletee)
+
+    item.appendChild(content)
+    item.appendChild(edits)
+
+    list.appendChild(item)
+
+    edit.addEventListener("click" , e => {
+      const input = content.querySelector("input[type='text']")
+      input.removeAttribute("readonly")
+      input.focus()
+      input.addEventListener("blur" , e => {
+        input.setAttribute("readonly" , true)
+        const taskIndex = tasks.indexOf(task);
+        tasks[taskIndex] = e.target.value;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        display();
+        })
+      })
+      deletee.addEventListener("click" , e => {
+        tasks = tasks.filter(t => t != task)
+        localStorage.setItem("tasks",JSON.stringify(tasks))
+        display()
+    })
+  });
+}
